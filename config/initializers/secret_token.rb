a9f7ac3bf9f909bf0912639f1fb4a8a50ba352c3
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-02-29 00:59:13 +0900

    Rails 3.2.1 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kilimanjaro::Application.config.secret_token = '6fda7ba3ced2ecafbb328c9ee3637cb1cfb99977f1ae14d04d663b76d27ca4a39749c5e9a16a6cd595a566907e071a48ed2451347d904e38e0ea3f0fd46cf133'
